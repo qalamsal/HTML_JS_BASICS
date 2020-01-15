b0e8580349e00dda5754aa46d4b366b6a3aa6e36
qalamsal8 <qalamsal8@gmail.com>
2020-01-14 23:40:25 -0600

    externaljs

---
+++ JS/Helper.js
@@ -0,0 +1,9 @@
+    function doHelp(){
+        var array = [];
+        array[0] = "Red";
+        array[1] = "Blue";
+        array[2] = "Green";
+        for (var i=0; i < array.length; i++){
+            alert(array[i]);
+        }
+    }
